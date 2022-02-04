ac0a164be911f0fc2c24a01472091d06daf1d7a3
erobinson <Edward.R.Robinson@gmail.com>
2022-02-04 06:11:58 -0500

    slightly better functional model

--- build_model.py
+++ build_model.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 
 class BuildModel(NightscoutMlBase):
 
-    def build_model(self, data_file):
+    def build_model(self, data_file, feature_cols, label_cols):
         print('tensorflow version:', tf.__version__)
         df = pandas.read_csv(self.data_folder+'/'+data_file)
         df = df.sample(frac=1).reset_index(drop=True)
@@ -44,29 +44,35 @@ class BuildModel(NightscoutMlBase):
         Y_test_tf = tf.convert_to_tensor(Y_test)
 
 
-        model = models.Sequential([
-            layers.Flatten(input_shape=(X_train.shape[1], 1)),
-            layers.Dense(128, activation='relu'),
-            layers.Dropout(0.2),
-            layers.Dense(Y_train.shape[1])
-        ])
+        # model = models.Sequential([
+        #     layers.Flatten(input_shape=(X_train.shape[1], 1)),
+        #     layers.Dense(128, activation='relu'),
+        #     layers.Dropout(0.2),
+        #     layers.Dense(Y_train.shape[1])
+        # ])
+
+        model = models.Sequential()
+        model.add(layers.Dense(X_train.shape[1], input_dim=X_train.shape[1], kernel_initializer='he_uniform', activation='relu'))
+        model.add(layers.Dense(10))
+        model.add(layers.Dense(Y_train.shape[1], activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer='adam')
 
         predictions = model(X_train_tf[:1]).numpy()
         print(predictions)
 
         # loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        loss_fn = losses.CategoricalCrossentropy()
-        initial_loss = loss_fn(Y_train_tf[:1], predictions).numpy()
-        print(initial_loss)
+        # loss_fn = losses.SparseCategoricalCrossentropy()
+        # initial_loss = loss_fn(Y_train_tf[:1], predictions).numpy()
+        # print(initial_loss)
 
-        model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
+        # model.compile(optimizer='adam',
+        #       loss=loss_fn,
+        #       metrics=['accuracy'])
 
-        model.fit(X_train_tf, Y_train_tf, epochs=5)
+        model.fit(X_train_tf, Y_train_tf, epochs=2)
 
         model.summary()
 
-        score = model.evaluate(X_test_tf,  Y_test_tf, verbose=2)
-        result = model.predict([[140]])
+        model.evaluate(X_test_tf,  Y_test_tf, verbose=5)
+        result = model.predict([[160]])
         print(result)
